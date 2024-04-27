aa9c042ef381cfb8e493682b95e42fc38f64b205
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-04-27 17:25:48 +0300

    Create a function that finds out Fibonacci's numbers and caches them

---
+++ hw01.py
@@ -0,0 +1,32 @@
+from typing import Callable
+
+
+def caching_fibonacci() -> Callable[[int], int]:
+    '''
+    Prepare cache for Fibonacci's numbers.
+
+    :return: Callable[[int], int]
+    '''
+
+    cache: dict[int, int] = {}
+
+    def fibonacci(number: int) -> int:
+        '''
+        Calculate Fibonacci's number and read proceed numbers from the cache.
+
+        :param number: int
+
+        :return: int
+        '''
+        
+        if number not in cache:
+            if number < 1:
+                cache[number] = 0
+            elif number > 1:
+                cache[number] = fibonacci(number - 1) + fibonacci(number - 2)
+            else:
+                cache[number] = 1
+        
+        return cache[number]
+    
+    return fibonacci
